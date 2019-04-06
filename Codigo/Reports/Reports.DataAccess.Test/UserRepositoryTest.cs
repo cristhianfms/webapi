391d7321d9e8c2a8442c9ddd7f597c55bd8a777f
Cristhian <maciel.cristhian@gmail.com>
2019-04-06 20:30:50 -0300

    Se agrega paquete DataAccess.Test y UserRepositoryTest.cs

---
+++ Codigo/Reports/Reports.DataAccess.Test/UserRepositoryTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reports.DataAccess.Test
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+
+        }
+
+
+    }
+}
