28a8e7243713d3ff852103897f469d59693f3a63
Cristhian <maciel.cristhian@gmail.com>
2019-04-16 22:23:16 -0300

    Pruebas unitarias correspondientes a Get y GetAll

--- Codigo/Reports/Reports.DataAccess.Test/UserRepositoryTest.cs
+++ Codigo/Reports/Reports.DataAccess.Test/UserRepositoryTest.cs
@@ -278,10 +278,164 @@ namespace Reports.DataAccess.Test
 
 
 
+        [TestMethod]
+        public void GetUserByID(){
+            string contextName = Guid.NewGuid().ToString();
+            var context = ContextFactory.GetMemoryContext(contextName);
+            IRepository<User> userRepo = new UserRepository(context);          
+            
+            var name = "Santiago";
+            var lastName = "Larralde";
+            var userName = "Santi1";
+            var password = "12345678";
+            var rol = User.UserType.M;
+            var id = Guid.NewGuid();
+
+            User user = new User{
+                Id = id,
+                Name = name,
+                LastName = lastName,
+                UserName = userName,
+                Password = password,
+               Rol = rol,
+            };
+
+            userRepo.Add(user); 
+            userRepo.Save();
+
+        
+            context = ContextFactory.GetMemoryContext(contextName);
+            userRepo = new UserRepository(context); 
+            User obtainedUser = userRepo.Get(id);
+
+            Assert.AreEqual(obtainedUser.Id, user.Id);
+        }
 
 
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void UpdateNotExistingUser(){
+           string contextName = Guid.NewGuid().ToString();
+            var context = ContextFactory.GetMemoryContext(contextName);
+            IRepository<User> userRepo = new UserRepository(context);          
+            
+            var name = "Santiago";
+            var lastName = "Larralde";
+            var userName = "Santi1";
+            var password = "12345678";
+            var rol = User.UserType.M;
+            var id = Guid.NewGuid();
+
+            User user = new User{
+                Id = id,
+                Name = name,
+                LastName = lastName,
+                UserName = userName,
+                Password = password,
+               Rol = rol,
+            };
+
+            userRepo.Update(user);
+            userRepo.Save();
+        }
 
 
+        [TestMethod]
+        public void GetAllUsers(){
+            var context = ContextFactory.GetMemoryContext(Guid.NewGuid().ToString());
+            IRepository<User> userRepo = new UserRepository(context);
+
+            var name = "Santiago";
+            var lastName = "Larralde";
+            var userName = "Santi1";
+            var password = "12345678";
+            var rol = User.UserType.M;
+            var id = Guid.NewGuid();
+
+            var name2 = "Cristhian";
+            var lastName2 = "Maciel";
+            var userName2 = "Cris1";
+            var password2 = "87654321";
+            var rol2 = User.UserType.M;
+            var id2 = Guid.NewGuid();
+
+            userRepo.Add(new User{
+                Id = id,
+                Name = name,
+                LastName = lastName,
+                UserName = userName,
+                Password = password,
+                Rol = rol,
+            }); 
+            
+            userRepo.Add(new User{
+                Id = id2,
+                Name = name2,
+                LastName = lastName2,
+                UserName = userName2,
+                Password = password2,
+                Rol = rol2,
+            });         
+            userRepo.Save();
+
+            List<User>  users = userRepo.GetAll().ToList();
+            Assert.AreEqual(users.Count(), 2);
+        }
+
+
+        [TestMethod]
+        public void GetAllUsers2(){
+            var context = ContextFactory.GetMemoryContext(Guid.NewGuid().ToString());
+            IRepository<User> userRepo = new UserRepository(context);
+
+            var name = "Santiago";
+            var lastName = "Larralde";
+            var userName = "Santi1";
+            var password = "12345678";
+            var rol = User.UserType.M;
+            var id = Guid.NewGuid();
+
+            var name2 = "Cristhian";
+            var lastName2 = "Maciel";
+            var userName2 = "Cris1";
+            var password2 = "87654321";
+            var rol2 = User.UserType.M;
+            var id2 = Guid.NewGuid();
+
+            userRepo.Add(new User{
+                Id = id,
+                Name = name,
+                LastName = lastName,
+                UserName = userName,
+                Password = password,
+                Rol = rol,
+            }); 
+
+            userRepo.Add(new User{
+                Id = id2,
+                Name = name2,
+                LastName = lastName2,
+                UserName = userName2,
+                Password = password2,
+                Rol = rol2,
+            });         
+            userRepo.Save();
+
+            List<User>  users = userRepo.GetAll().ToList();
+            Assert.IsTrue(users[0].Id == id && users[1].Id == id2);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void GetNotExistingId(){
+            string contextName = Guid.NewGuid().ToString();
+            var context = ContextFactory.GetMemoryContext(contextName);
+            IRepository<User> userRepo = new UserRepository(context);          
+        
+            Guid id = Guid.NewGuid();
+            User obtainedUser = userRepo.Get(id);
+        }
 
     }
 }
--- Codigo/Reports/Reports.DataAccess/RepositoryException.cs
+++ Codigo/Reports/Reports.DataAccess/RepositoryException.cs
@@ -4,6 +4,7 @@ namespace Reports.DataAccess
 {
     public class RepositoryException : Exception
     {
+        public RepositoryException(string message): base(message){}
         public RepositoryException(string message, Exception inner) :base(message, inner){}
     }
 
