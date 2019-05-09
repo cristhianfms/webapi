2099ed64ffe5b28ac428a88fa4849a8c91b051c1
Cristhian <maciel.cristhian@gmail.com>
2019-05-09 10:31:38 -0300

    Se crea clase DBConnection para realizar consultas sql a partir de un connection string

---
+++ Codigo/Reports/Reports.DataAccess.Interface/IDBConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Reports.DataAccess.Interface
+{
+    public interface IDBConnection
+    {
+        DataSet GetResult();
+    }
+}
---
+++ Codigo/Reports/Reports.DataAccess/DBConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Reports.DataAccess.Interface;
+using System.Data.SqlClient;
+
+namespace Reports.DataAccess
+{
+    public class DBConnection : IDBConnection
+    {
+        public string QuerySQL { get; set; }
+        public string ConnectionString { get; set; }
+
+        public DBConnection(string connectionString, string querySQL)
+        {
+            this.QuerySQL = querySQL;
+            this.ConnectionString = connectionString;
+        }
+
+        public DataSet GetResult()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(ConnectionString);
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(QuerySQL, connection);
+                DataSet dSet = new DataSet();
+                dataAdapter.Fill(dSet);
+                return dSet;
+            }
+            catch (Exception e)
+            {
+                throw new RepositoryException("Error de conexión a la base",e);
+            }
+
+        }
+    }
+}
